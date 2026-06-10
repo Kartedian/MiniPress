d43fe5c603e4886fa02ff23901a843563cb7cdac
viper <walid.bouaoukel2@etu.univ-lorraine.fr>
2026-06-10 12:40:11 +0200

    [add] AuthnService

---
+++ MiniPress.core/src/application_core/application/usecases/AuthnService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Dwm\MiniPress\application_core\application\usecases;
+
+use Dwm\MiniPress\application_core\application\usecases\AuthnServiceInterface;
+use Dwm\MiniPress\infrastructure\User;
+
+class AuthnService implements AuthnServiceInterface
+{
+
+}
