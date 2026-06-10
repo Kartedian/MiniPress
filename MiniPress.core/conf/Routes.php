e6995e1cd433fadc95ea4be5932802d09aac811b
MatteoGren <matteog57@outlook.fr>
2026-06-10 22:35:33 +0200

    ajout d'un affichage dans la class route

--- MiniPress.core/conf/Routes.php
+++ MiniPress.core/conf/Routes.php
@@ -1,7 +1,14 @@
 <?php
 
 use Slim\App;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
 return function (App $app): App{
+    $app->get('/', function (Request $request, Response $response) {
+        $response->getBody()->write("<h1>Welcome to MiniPress</h1>");
+        return $response;
+    });
+
     return $app;
 };
