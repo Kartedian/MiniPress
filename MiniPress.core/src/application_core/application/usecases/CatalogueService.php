40c50201be8563fe5897185b446b35d1307c76f1
viper <walid.bouaoukel2@etu.univ-lorraine.fr>
2026-06-10 12:41:23 +0200

    [add] CatalogueService

---
+++ MiniPress.core/src/application_core/application/usecases/CatalogueService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Dwm\MiniPress\application_core\application\usecases;
+
+use Dwm\MiniPress\application_core\application\usecases\CatalogueServiceInterface;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+class CatalogueService implements CatalogueServiceInterface
+{
+
+}
