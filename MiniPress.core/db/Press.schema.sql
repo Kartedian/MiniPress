e530cbf24e813d03df7d4c725893a373fd117269
MatteoGren <matteog57@outlook.fr>
2026-06-10 11:23:47 +0200

    ajout des fichier pour la bd

---
+++ MiniPress.core/db/Press.schema.sql
@@ -0,0 +1,55 @@
+-- Adminer 4.17.1 MySQL 11.7.2-MariaDB-ubu2404 dump
+use giftbox;
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+DROP TABLE IF EXISTS `Article`;
+CREATE TABLE `box` (
+  `id` varchar(128) NOT NULL,
+  `Titre` varchar(64) NOT NULL,
+  `resumer` text NOT NULL,
+  `Contenue` text DEFAULT NULL,
+  `Date` DATETIME default ,
+  `Categorie` tinyint(4) NOT NULL DEFAULT 0,
+  `ID-Auteur` varchar(128) NOT NULL,
+  `Etats` INT NOT NULL DEFAULT 0,
+   PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+
+DROP TABLE IF EXISTS `categorie`;
+CREATE TABLE `categorie` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `libelle` varchar(128) NOT NULL,
+  `description` text DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+
+SET NAMES utf8mb4;
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` varchar(40) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `user_id` varchar(128) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `password` varchar(256) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `role` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_pk2` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- DROP TABLE IF EXISTS `user2article`;
+-- CREATE TABLE `user2article` (
+--   `id` int(11) NOT NULL AUTO_INCREMENT,
+--   `user_id` varchar(40) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+--   `article_id` varchar(128) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+--   PRIMARY KEY (`id`),
+--   KEY `user_id` (`user_id`),
+--   KEY `article_id` (`article_id`),
+--   CONSTRAINT `user2article_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+--   CONSTRAINT `user2article_ibfk_2` FOREIGN KEY (`article_id`) REFERENCES `box` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
