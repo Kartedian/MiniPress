07dff2755a1aad01be4359f005ab25c7229862bf
viper <walid.bouaoukel2@etu.univ-lorraine.fr>
2026-06-11 10:44:58 +0200

    [update] changed  type + added

--- MiniPress.core/src/application_core/domain/entities/ArticleEntity.php
+++ MiniPress.core/src/application_core/domain/entities/ArticleEntity.php
@@ -2,14 +2,17 @@
 
 namespace Dwm\MiniPress\application_core\domain\entities;
 
+use Illuminate\Support\Facades\Date;
+
 class ArticleEntity{
     public function __construct(
         public readonly string $id,
         public readonly string $titre,
         public readonly ?string $resumer,
         public readonly ?string $contenue,
-        public readonly \DateTimeImmutable $date,
+        public readonly Date $date,
         public readonly int $categorie,
+        public readonly string $url_image,
         public readonly string $id_Auteur,
         public readonly int $published
     ) {}
