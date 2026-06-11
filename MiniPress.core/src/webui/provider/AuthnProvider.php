5741c36a3293cd816fd195b2f4b8898ac4ea0450
viper <walid.bouaoukel2@etu.univ-lorraine.fr>
2026-06-11 10:24:28 +0200

    [add] AuthnProviderService + CsrfTokenProvider

---
+++ MiniPress.core/src/webui/provider/AuthnProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Dwm\MiniPress\webui\provider;
+
+use Dwm\MiniPress\application_core\application\usecases\AuthnService;
+
+class AuthnProvider
+{
+    public static function register(string $email, string $password): bool
+    {
+        return AuthnService::register($email, $password);
+    }
+
+    public static function authenticate(string $email, string $password): bool
+    {
+        $res = AuthnService::authenticate($email, $password);
+        if ($res) {
+            $_SESSION['user_id'] = $res['user_id'];
+            $_SESSION['role'] = $res['role'];
+            return true;
+        }
+        return false;
+    }
+
+    public static function logout(): void
+    {
+        $_SESSION = [];
+        session_destroy();
+    }
+
+    public static function isAuthenticated(): bool
+    {
+        return isset($_SESSION['user_id']);
+    }
+
+    public static function getUserId(): ?String
+    {
+        return $_SESSION['user_id'] ?? null;
+    }
+
+    public static function getUserRole(): ?int
+    {
+        return $_SESSION['role'] ?? null;
+    }
+}
