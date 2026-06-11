e67580472ca2608f2d5a54c1cd8dd932ff1b1a4e
Jules.Adr <jules.adr@orange.fr>
2026-06-11 09:08:43 +0200

    Fonctionnalité 1 : Création d'un article

--- MiniPress.core/conf/Routes.php
+++ MiniPress.core/conf/Routes.php
@@ -1,14 +1,26 @@
 <?php
 
-use Slim\App;
+use Dwm\MiniPress\application_core\application\usecases\CatalogueService;
+use Dwm\MiniPress\webui\ArticleController;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\App;
+use Slim\Views\Twig;
 
-return function (App $app): App{
-    $app->get('/', function (Request $request, Response $response) {
-        $response->getBody()->write("<h1>Welcome to MiniPress</h1>");
-        return $response;
+return function (App $app): App {
+
+    $catalogueService  = new CatalogueService();
+    $articleController = new ArticleController($catalogueService);
+
+    //Accueil
+    $app->get('/', function (Request $request, Response $response): Response {
+        $view = Twig::fromRequest($request);
+        return $view->render($response, 'index.twig');
     });
+
+    //Articles
+    $app->get('/articles/creer', [$articleController, 'showForm']);
+    $app->post('/articles/creer', [$articleController, 'traiterForm']);
 
     return $app;
 };
--- MiniPress.core/src/application_core/application/usecases/CatalogueServiceInterface.php
+++ MiniPress.core/src/application_core/application/usecases/CatalogueServiceInterface.php
@@ -2,7 +2,19 @@
 
 namespace Dwm\MiniPress\application_core\application\usecases;
 
+use Dwm\MiniPress\application_core\domain\entities\ArticleEntity;
+use Dwm\MiniPress\application_core\domain\entities\CategorieEntity;
+
 interface CatalogueServiceInterface
 {
-    
+    /** @return CategorieEntity[] */
+    public function listerCategories(): array;
+
+    public function creerArticle(
+        string $titre,
+        ?string $resumer,
+        ?string $contenue,
+        int $categorie,
+        string $idAuteur
+    ): ArticleEntity;
 }
