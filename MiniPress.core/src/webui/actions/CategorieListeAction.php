f7f615a8dcec1a52b06613592870a670a97898af
viper <walid.bouaoukel2@etu.univ-lorraine.fr>
2026-06-12 14:01:06 +0200

    [feature] add isAuthenticated

--- MiniPress.core/src/webui/actions/CategorieListeAction.php
+++ MiniPress.core/src/webui/actions/CategorieListeAction.php
@@ -4,7 +4,7 @@ namespace Dwm\MiniPress\webui\actions;
 
 use Dwm\MiniPress\application_core\application\usecases\DatabaseServiceInterface;
 use Dwm\MiniPress\application_core\domain\exceptions\DatabaseException;
-use Dwm\MiniPress\webui\provider\AuthnProvider;
+use Dwm\MiniPress\webui\provider\AuthProviderInterface;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Exception\HttpInternalServerErrorException;
@@ -12,7 +12,7 @@ use Slim\Views\Twig;
 
 class CategorieListeAction
 {
-    public function __construct(private readonly DatabaseServiceInterface $db){}
+    public function __construct(private readonly DatabaseServiceInterface $db, private readonly AuthProviderInterface $auth){}
 
     public function __invoke(Request $request, Response $response, array $args){
         try{
@@ -22,9 +22,13 @@ class CategorieListeAction
             throw new HttpInternalServerErrorException($request, $e->getMessage());
         }
 
+        $isAuthenticated = $this->auth::isAuthenticated();
+
+
         $view = Twig::fromRequest($request);
         return $view->render($response, 'CategorieListeView.html', [
             'categories' => $categories,
+            'isAuthenticated' => $isAuthenticated
         ]);
     }
 }
