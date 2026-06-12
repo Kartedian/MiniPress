ae6e74947bde1382899487e0dc17773b0b5bc043
viper <walid.bouaoukel2@etu.univ-lorraine.fr>
2026-06-12 13:59:27 +0200

    [update] fix erreur + add auteur

--- MiniPress.core/src/webui/actions/ArticleDetailAction.php
+++ MiniPress.core/src/webui/actions/ArticleDetailAction.php
@@ -2,9 +2,10 @@
 
 namespace Dwm\MiniPress\webui\actions;
 
+use Dwm\MiniPress\application_core\application\usecases\AuthnServiceInterface;
 use Dwm\MiniPress\application_core\application\usecases\DatabaseServiceInterface;
 use Dwm\MiniPress\application_core\domain\exceptions\DatabaseException;
-use Dwm\MiniPress\webui\provider\AuthnProvider;
+use Dwm\MiniPress\webui\provider\AuthProviderInterface;
 use Dwm\MiniPress\webui\provider\CsrfTokenProvider;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
@@ -13,7 +14,7 @@ use Slim\Views\Twig;
 
 class ArticleDetailAction
 {
-    public function __construct(private readonly DatabaseServiceInterface $db) {}
+    public function __construct(private readonly DatabaseServiceInterface $db, private readonly AuthProviderInterface $auth, private readonly AuthnServiceInterface $authS) {}
 
     public function __invoke(Request $request, Response $response, array $args)
     {
@@ -23,16 +24,19 @@ class ArticleDetailAction
             throw new HttpInternalServerErrorException($request, $e->getMessage());
         }    
 
-        $currentUserId = AuthnProvider::getUserId();
+        $currentUserId = $this->auth->getUser();
+        
+        $isOwner = ($currentUserId !== null && $currentUserId->id === $article['id_auteur']);
 
-        $isOwner = ($currentUserId !== null && $currentUserId === $article['id_auteur']);
-
+        $auteur = $this->authS->getNameUserById($article['id_auteur']);
+        
         $csrfToken = CsrfTokenProvider::generateToken();
         $view = Twig::fromRequest($request);
         return $view->render($response, 'ArticleDetailView.html', [
             'article' => $article,
             'is_owner' => $isOwner,
-            'csrf_token' => $csrfToken
+            'csrf_token' => $csrfToken,
+            'auteur' => $auteur
         ]);
     }
 }
