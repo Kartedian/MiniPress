7a7b6bbdd7e793521f39c370bd39a3faa7c9e9da
viper <walid.bouaoukel2@etu.univ-lorraine.fr>
2026-06-12 13:57:28 +0200

    [feature] add name

--- MiniPress.core/src/application_core/domain/entities/UserEntity.php
+++ MiniPress.core/src/application_core/domain/entities/UserEntity.php
@@ -4,7 +4,8 @@ namespace Dwm\MiniPress\application_core\domain\entities;
 
 class UserEntity{
     public function __construct(
-        public readonly int $id,
+        public readonly string $id,
+        public readonly string $name,
         public readonly string $user_id,
         public readonly string $password,
         public readonly int $Role
