1385ea3b93cbf3c17c00d8eb88fa2ab0ff14266b
viper <walid.bouaoukel2@etu.univ-lorraine.fr>
2026-06-12 14:00:21 +0200

    [feature] add MesListeArticle

--- MiniPress.core/src/webui/actions/ArticleListeAction.php
+++ MiniPress.core/src/webui/actions/ArticleListeAction.php
@@ -4,6 +4,7 @@ namespace Dwm\MiniPress\webui\actions;
 
 use Dwm\MiniPress\application_core\application\usecases\DatabaseServiceInterface;
 use Dwm\MiniPress\application_core\domain\exceptions\DatabaseException;
+use Dwm\MiniPress\webui\provider\AuthProviderInterface;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Exception\HttpInternalServerErrorException;
@@ -11,22 +12,30 @@ use Slim\Views\Twig;
 
 class ArticleListeAction
 {
-    public function __construct(private readonly DatabaseServiceInterface $db){}
+    public function __construct(private readonly DatabaseServiceInterface $db, private readonly AuthProviderInterface $auth){}
 
     public function __invoke(Request $request, Response $response, array $args){
         $queryParams = $request->getQueryParams();
         $categorieId = $queryParams['categorie'] ?? null;
+
+        $mesArticles = []; 
+        $isAuthenticated = $this->auth->isAuthenticated();
+
         try{
             $categories = $this->db->getCategories();
 
             if(!empty($categorieId)){
                 $articles = $this->db->getArticlesFromCategory((int)$categorieId);
+            } else {
+                $articles = $this->db->getArticles(); 
             }
-            else{
-                $articles = $this->db->getArticles();
+
+            if ($isAuthenticated) {
+                $userId = $this->auth->getUserId();
+                $mesArticles = $this->db->getArticlesByIdAuteur($userId);
             }
-        }
-        catch(DatabaseException $e){
+
+        } catch(DatabaseException $e){
             throw new HttpInternalServerErrorException($request, $e->getMessage());
         }
 
@@ -35,6 +44,8 @@ class ArticleListeAction
             'articles' => $articles,
             'categories' => $categories,
             'current_cat' => $categorieId,
+            'isAuthenticated' => $isAuthenticated,
+            'mes_articles' => $mesArticles
         ]);
     }
 }
