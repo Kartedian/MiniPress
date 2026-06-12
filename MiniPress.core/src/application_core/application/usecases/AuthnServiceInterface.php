37a567abdef9b1b503a879378037a8d30436a5a4
walid <walid.bouaoukel2@etu.univ-lorraine.fr>
2026-06-12 02:02:00 +0200

    [fix] static function (not complete)

--- MiniPress.core/src/application_core/application/usecases/AuthnServiceInterface.php
+++ MiniPress.core/src/application_core/application/usecases/AuthnServiceInterface.php
@@ -5,9 +5,9 @@ namespace Dwm\MiniPress\application_core\application\usecases;
 use Dwm\MiniPress\application_core\domain\entities\UserEntity;
 
 interface AuthnServiceInterface {
-    public function register(string $email, string $password): UserEntity;
+    public static function register(string $email, string $password): UserEntity;
 
-    public function login(string $email, string $password): ?UserEntity;
+    public static function login(string $email, string $password): ?UserEntity;
 
-    public function getUserById(string $userId): ?UserEntity;
+    public static function getUserById(string $userId): ?UserEntity;
 }
