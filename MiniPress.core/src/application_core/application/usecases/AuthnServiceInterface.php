424129c52a45236876aa4c0c156dda1a4b9711af
viper <walid.bouaoukel2@etu.univ-lorraine.fr>
2026-06-12 12:49:52 +0200

    [feature] add name to register

--- MiniPress.core/src/application_core/application/usecases/AuthnServiceInterface.php
+++ MiniPress.core/src/application_core/application/usecases/AuthnServiceInterface.php
@@ -5,7 +5,7 @@ namespace Dwm\MiniPress\application_core\application\usecases;
 use Dwm\MiniPress\application_core\domain\entities\UserEntity;
 
 interface AuthnServiceInterface {
-    public static function register(string $email, string $password): UserEntity;
+    public static function register(string $name, string $email, string $password): UserEntity;
 
     public static function login(string $email, string $password): ?UserEntity;
 
